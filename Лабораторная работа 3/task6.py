c44cd282d5e21068662c18c58afb50d872095d8a
Артур Никитин <115408648+nikitin2ag@users.noreply.github.com>
2022-10-16 00:07:45 +0300

    Update task6.py

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -1,13 +1,11 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-#находим максимальный элемент
+#находим индекс максимального элемента
 max_number = list_numbers[0]
-for number in list_numbers:
+for i, number in enumerate(list_numbers):
     if number > max_number:
         max_number = number
-
-#получаем индекс максимального элемента
-index = list_numbers.index(max_number)
+        index = i
 
 #меняем местами максимальный и последний элементы
 list_numbers[index], list_numbers[-1] = list_numbers[-1], list_numbers[index]
