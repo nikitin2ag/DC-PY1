416661ceecad7799f47bfef5b364698636c17677
Артур Никитин <115408648+nikitin2ag@users.noreply.github.com>
2022-11-14 23:01:24 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -2,9 +2,9 @@ import string
 from random import sample
 
 
-def get_random_password(n=8) -> str:
+def get_random_password(size=8) -> str:
     strings = string.ascii_letters + string.digits
-    return "".join(sample(strings, n))
+    return "".join(sample(strings, size))
 
 
 print(get_random_password())
