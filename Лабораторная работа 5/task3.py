6b27c61ee53909738e8276f38a558f49e015736f
Артур Никитин <115408648+nikitin2ag@users.noreply.github.com>
2022-11-14 23:00:00 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -3,6 +3,10 @@ from random import randint
 
 def get_unique_list_numbers(start=-10, end=10, size=15) -> list[int]:
     unique_list_numbers = []
+    if size > end - start + 1:
+        raise ValueError("Размер списка должен быть не больше количества целых чисел в диапазоне!")
+    if start > end:
+        raise ValueError('Начало диапазона должно быть не больше конца!')
     while len(unique_list_numbers) < size:
         random_number = randint(start, end)
         if random_number not in unique_list_numbers:
@@ -10,6 +14,6 @@ def get_unique_list_numbers(start=-10, end=10, size=15) -> list[int]:
     return unique_list_numbers
 
 
-list_unique_numbers = get_unique_list_numbers()
+list_unique_numbers = get_unique_list_numbers(2, 10, 3)  # в скобках можно указать произвольные значения (start, end, size)
 print(list_unique_numbers)  # выводим список из 15 случайных неповторяющихся целых чисел от -10 до 10 включительно
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))  # True
