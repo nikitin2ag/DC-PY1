eb2f5cd41968ee99db469a56ea9f85927eaf782a
Артур Никитин <115408648+nikitin2ag@users.noreply.github.com>
2022-12-01 10:49:17 +0300

    Update task4.py

--- Лабораторная работа 6/task4.py
+++ Лабораторная работа 6/task4.py
@@ -4,14 +4,12 @@ INPUT_FILE = "input.csv"
 
 
 def csv_to_list_dict(file, delimiter=',', new_line='\n') -> list[dict]:
-    # TODO реализовать конвертер из csv в json
     with open(file) as f:
         header = f.readline().replace(new_line, '').split(delimiter)
+        output_str = []
         for line in f:
             values = line.replace(new_line, '').split(delimiter)
-        output_str = []
-        for i, s in enumerate(values):
-            output_str.append(dict(zip(header, values[i])))
+            output_str.append(dict(zip(header, values)))
         return output_str
 
 
